3c7b36cc70331083089e9b35dfc5c43e339ace62
jhonatanlinux <jhonatan135135@gmail.com>
2023-09-13 17:22:14 -0300

    seção 13

--- src/components/FinanceControl/FinanceControl.tsx
+++ src/components/FinanceControl/FinanceControl.tsx
@@ -1,6 +1,7 @@
 import { FinanceControlProps } from "../../models/interfaces/FinanceControlProps/FinanceControlProps";
 import { Movement } from "../../models/interfaces/Movement/Movement";
 import Balance from "../Balance/Balance";
+import Expense from "../Expenses/Expense";
 import "./FinanceControl.css";
 
 const FinanceControl = ({
@@ -14,7 +15,11 @@ const FinanceControl = ({
   return (
     <div className="container_finances">
       <Balance currentBalance={balance} emitMovement={receiveNewMovement} />
-      {/* EXPENSE */}
+      <Expense
+        currentBalance={balance}
+        currentExpenses={expenses}
+        emitMovement={receiveNewMovement}
+      />
     </div>
   );
 };
