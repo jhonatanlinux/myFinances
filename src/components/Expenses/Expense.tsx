da904e94a3f878a7cb4ed4d4e0b654f0b229b06a
jhonatanlinux <jhonatan135135@gmail.com>
2023-09-15 09:40:20 -0300

    seção 17 - ok

--- src/components/Expenses/Expense.tsx
+++ src/components/Expenses/Expense.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import "./Expense.css";
 import { ExpenseProps } from "../../models/interfaces/ExpenseProps/ExpenseProps";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPercent } from "@fortawesome/free-solid-svg-icons";
 import Button from "../Button/Button";
 import { FormatMoney } from "../../utils/util";
+import { ActionsContainer, Card, CardHeader, Container, FormContainer, FormInput } from "../Balance/Balance";
 
 const Expense = ({
   emitMovement,
@@ -59,12 +59,12 @@ const Expense = ({
 
   return (
     <div>
-      <div className="expense_container">
-        <div className="expense_card">
-          <header className="expense_header">
+      <Container>
+        <Card>
+          <CardHeader>
             <FontAwesomeIcon icon={faPercent} color="#e43f4d" size="2x" />
             <h2>DESPESAS</h2>
-          </header>
+          </CardHeader>
           <h3>
             {currentExpenses > 0
               ? FormatMoney(String(currentExpenses))
@@ -80,38 +80,32 @@ const Expense = ({
           )}
           {renderInputForm && (
             <form onSubmit={formSubmitHandler}>
-              <div
-                className={`input_form_container ${
-                  !isFormValid ? "invalid" : ""
-                }`}
-              >
-                <input
+              <FormContainer invalid={!isFormValid}>
+                <FormInput
                   type="text"
                   placeholder="Nome"
-                  className="expense_input"
                   value={inputName}
                   onChange={handleInputNameForm}
                 />
-                <input
+                <FormInput
                   type="text"
                   placeholder="Valor"
-                  className="expense_input"
                   value={inputValue}
                   onChange={handleInputValueForm}
                 />
-              </div>
-              <div className="actions_form_buttons_container">
+              </FormContainer>
+              <ActionsContainer>
                 <Button
                   title="Cancelar"
                   priority="Output"
                   action={hideInputForm}
                 />
                 <Button type="submit" title="Adicionar" priority="Input" />
-              </div>
+              </ActionsContainer>
             </form>
           )}
-        </div>
-      </div>
+        </Card>
+      </Container>
     </div>
   );
 };
